d76e0700c3b0cf472dea0b6118133780e7fb7aed
Yuichiro Tsuji <y.tsuji@nac-care.co.jp>
2020-08-03 14:58:58 +0900

    Modify to get conversion.

--- product_detail.php
+++ product_detail.php
@@ -74,13 +74,14 @@ var ExpireDays = 90;
 //Do not change anything below this line
 qstr = document.location.search;
 qstr = qstr.substring(1,qstr.length)
+alert("qstr=" + qstr);
 function SetCookie(cookieName,cookieValue,nDays)
 {
+alert("SetCookie: " + cookieName + "=" + cookieValue);
 	var today = new Date();
 	var expire = new Date();
 	if (nDays==null || nDays==0) nDays=1;
 	expire.setTime(today.getTime() + 3600000*24*nDays);
-alert("SetCookie: " + cookieName + "=" + cookieValue);
 	document.cookie = cookieName+"="+escape(cookieValue)+ ";expires="+expire.toGMTString();
    }
    

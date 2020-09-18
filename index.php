2bde280a3a61963aeecc6181c4fc47e5285c8026
Yuichiro Tsuji <y.tsuji@nac-care.co.jp>
2020-09-18 11:42:18 +0900

    cotta Test 20200918

--- index.php
+++ index.php
@@ -12,6 +12,49 @@ require_once('include/inc_php_start.php');
 </head><!-- メタ情報やCSSのリンク先を読み込み -->
 
 <body>
+<script language="javascript">
+//Set the number of days before your cookie should expire
+var ExpireDays = 90;
+//Do not change anything below this line
+qstr = document.location.search;
+qstr = qstr.substring(1,qstr.length)
+function SetCookie(cookieName,cookieValue,nDays)
+{
+	var today = new Date();
+	var expire = new Date();
+	if (nDays==null || nDays==0) nDays=1;
+	expire.setTime(today.getTime() + 3600000*24*nDays);
+	document.cookie = cookieName+"="+escape(cookieValue)+ ";domain=cotta.jp;expires="+expire.toGMTString();
+	document.cookie = cookieName+"="+escape(cookieValue)+ ";domain=www.cotta.jp;expires="+expire.toGMTString();
+}
+
+thevars = qstr.split("&");
+for(i=0;i<thevars.length;i++) {
+	var element = thevars[i].split('=');
+	switch(element[0]) {
+		case "sfmc_sub":
+			SetCookie("SubscriberID",element[1],ExpireDays);
+			break;
+		case "j":
+			SetCookie("JobID",element[1],ExpireDays);
+			break;
+		case "l":
+			SetCookie("ListID",element[1],ExpireDays);
+			break;
+		case "u":
+			SetCookie("UrlID",element[1],ExpireDays);
+			break;
+		case "jb":
+			SetCookie("BatchID",element[1],ExpireDays);
+			break;
+		case "mid":
+			SetCookie("MemberID",element[1],ExpireDays);
+			break;
+		default:
+			break;
+	}
+}
+</script>
   <header><?php include('include/inc_tag_header.php'); ?></header><!-- ヘッダーの読み込み -->
   <nav><?php include('include/inc_tag_nav.php'); ?></nav><!-- ナビゲーションの読み込み -->
 

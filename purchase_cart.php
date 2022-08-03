605fb3ff43ae8137cb85f4071c70f8caae0263e3
Yuichiro Tsuji <y.tsuji@nac-care.co.jp>
2022-08-03 17:52:10 +0900

    MCP対応（カート画面）

--- purchase_cart.php
+++ purchase_cart.php
@@ -7,7 +7,7 @@ if (empty($_SESSION['member_info'])) {
 }
 
 $member_id = $_SESSION['member_info']['member_id'];
-$message = '商品が追加されていません。';
+$message = '<div class=".cart-empty">商品が追加されていません。</div>';
 
 $cart_list = $obj->get_cart_list($member_id);
 $cart_total_price = $obj->get_cart_total_price($member_id);
@@ -28,7 +28,7 @@ $cart_total_price = $obj->get_cart_total_price($member_id);
   <main>
     <h1 class="headline"><span>CART</span></h1>
     <div class="msg"><span><?php if (empty($cart_total_price['sum'])) echo $message; ?></span></div>
-    <div class="pc-container">
+    <div class="pc-container product-info">
 
       <?php if (!empty($cart_total_price['sum'])) : ?>
         <div class="pc-container-elem">
@@ -46,7 +46,7 @@ $cart_total_price = $obj->get_cart_total_price($member_id);
 
         <?php foreach ($cart_list as $row) : ?>
           <?php $product_detail = $obj->get_product_detail($row['product_id']); ?>
-          <div class="pc-container-elem">
+          <div class="pc-container-elem product-details">
             <div class="pc-container-elem-item">
               <div class="item-box-left">
                 <a href="product_detail.php?id=<?= $obj->h($row['product_id']); ?>">
@@ -57,8 +57,8 @@ $cart_total_price = $obj->get_cart_total_price($member_id);
                 <div class="item-pid"><?= $obj->h($row['product_id']); ?></div>
                 <div class="item-name"><?= $obj->h($product_detail['product_name']); ?></div>
                 <div class="item-price">&yen;<?= $obj->h($product_detail['price']); ?></div>
-                <div class="item-num">個数 <?= $obj->h($row['number']); ?></div>
-                <div class="item-num"><input type="button" onclick="location.href='purchase_cart_delete.php?id=<?= $obj->h($row['product_id']); ?>'" value="削除"></div>
+                <div class="item-num ">個数 <?= $obj->h($row['number']); ?></div>
+                <div class="item-num-btn"><input type="button" onclick="location.href='purchase_cart_delete.php?id=<?= $obj->h($row['product_id']); ?>'" value="削除" class="btn btn-del"></div>
               </div>
             </div>
           </div>
@@ -69,7 +69,7 @@ $cart_total_price = $obj->get_cart_total_price($member_id);
             <div class="item-box-left"></div>
             <div class="item-box-right">
               <div class="container-btn">
-                <input type="button" onclick="delete_cart(); location.href='purchase_cart_delete.php'" value="全削除" class="btn">
+                <input type="button" onclick="delete_cart(); location.href='purchase_cart_delete.php'" value="全削除" class="btn btn-all-del">
               </div>
             </div>
           </div>
